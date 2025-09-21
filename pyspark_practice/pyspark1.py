cde37b7e150b75bd4f60cb6e97b3f4ce3b19c7f1
chinnuneela <chinnuneela@gmail.com>
2025-09-21 12:38:55 +0530

    sync 09/21

--- pyspark_practice/pyspark1.py
+++ pyspark_practice/pyspark1.py
@@ -2,12 +2,15 @@ from pyspark.sql import SparkSession
 
 data = [1,2,3,4]
 
-spark = SparkSession.builder.appName("parallelizeexample").getOrCreate()
+spark = SparkSession.builder \
+.appName("parallelizeexample") \
+.getOrCreate()
 
 sc = spark.sparkContext
 rdd = sc.parallelize(data)
 
-print("RDD contents:", rdd.collect())
+print("RDD contents:", \
+      rdd.collect())
 
 spark.stop()
 
